92b63d3f753067588c4b94691b5f6744031caa51
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-09 20:09:27 +0530

    Added : Validating Users Last Name

--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 public class UserRegistrationMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Your Choice \n1.Validate First Name ");
+        System.out.println("Enter Your Choice \n1.Validate First Name  \n2.Validate Last Name");
         int Option = scanner.nextInt();
         switch (Option) {
             case 1:
                 UserRegistration.validFirstName();
+                break;
+            case 2:
+                UserRegistration.validLastName();
                 break;
             default:
                 System.out.println("Enter Valid Choice");
