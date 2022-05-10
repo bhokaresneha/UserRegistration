7f62a5758432dc04d4b5e47086c0e66385c50475
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-10 08:47:01 +0530

    Added : Validating Email Address

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static final Scanner scanner = new Scanner(System.in);
+    static boolean r;
 
     public static boolean validFirstName() {
         System.out.print("Enter the First Name (First Latter Must be Capital): ");
@@ -13,7 +14,7 @@ public class UserRegistration {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(name);
-        boolean r = m.matches();
+         r = m.matches();
 
         if (r)
             System.out.println("First name is valid");
@@ -21,5 +22,44 @@ public class UserRegistration {
             System.out.println("First name is !Invalid");
         return r;
     }
+
+    public static boolean validLastName()
+    {
+        System.out.print("Enter the Last Name (First Latter Must be Capital): ");
+        String name = scanner.next();
+
+        if (name.length() >= 3 )
+        {
+            String regex = "^[A-Z]{1}[a-z]{2,}$";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(name);
+            r = m.matches();
+            if (r)
+                System.out.println("Last Name is valid");
+            else
+                System.out.println("Last Name is !Invalid");
+        } else
+        {
+            System.out.print(" Minimum 3 Character Required");
+        }
+        return r;
+    }
+
+    public static boolean validEmail()
+    {
+        System.out.print("Enter the Email : ");
+        String email = scanner.next();
+        String regex = "^[a-z0-9]+@[a-z0-9]+.[a-z]{2,3}+.[a-z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        boolean r = m.matches();
+
+        if (r)
+            System.out.println("Email is valid.");
+        else
+            System.out.println("Email is invalid.");
+        return r;
+    }
+
 
 }
--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -4,15 +4,25 @@ import java.util.Scanner;
 public class UserRegistrationMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Your Choice \n1.Validate First Name ");
-        int Option = scanner.nextInt();
-        switch (Option) {
-            case 1:
-                UserRegistration.validFirstName();
-                break;
-            default:
-                System.out.println("Enter Valid Choice");
-
-        }
+        int choice;
+        do {
+            System.out.println("Enter Your Choice \n1.Validate First Name  \n2.Validate Last Name \n3.Validate Email");
+            int Option = scanner.nextInt();
+            switch (Option) {
+                case 1:
+                    UserRegistration.validFirstName();
+                    break;
+                case 2:
+                    UserRegistration.validLastName();
+                    break;
+                case 3:
+                    UserRegistration.validEmail();
+                    break;
+                default:
+                    System.out.println("Enter Valid Choice");
+            }
+            System.out.println("If you want to perform more validations then press 1");
+            choice = UserRegistration.scanner.nextInt();
+        } while (choice == 1);
     }
 }
