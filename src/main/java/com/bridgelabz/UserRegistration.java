fe413d60ffe3b293c4b7cb8af83c9acaaa6cf57f
bhokaresneha <bhokaresneha20@gmail.com>
2022-05-16 22:58:47 +0530

    Added : validate_multiple_entry_for_the_Email_Address.

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistration {
+public class UserRegistration
+{
     public static final Scanner scanner = new Scanner(System.in);
     static boolean r;
 
-    public static boolean validFirstName(String fname) throws InvalidUserDataException {
+    public static boolean validFirstName(String fname)
+    {
             /*    System.out.print("Enter the First Name (First Latter Must be Capital ): ");
                     String name = scanner.next();
                 */
@@ -31,7 +33,8 @@ public class UserRegistration {
 
     }
 
-    public static boolean validLastName(String lastname)throws InvalidUserDataException {
+    public static boolean validLastName(String lastname)
+    {
              /*   System.out.print("Enter the Last Name (First Latter Must be Capital): ");
                 String lastname = scanner.next();
 */
@@ -46,12 +49,13 @@ public class UserRegistration {
         return r1;
     }
 
-    public static boolean validEmail(String email)throws InvalidUserDataException {
+    public static boolean validEmail(String email)
+    {
                 /*System.out.print("Enter the Email : ");
                 String email = scanner.next();
                 */
         //sneha_90@hotmail.com.in
-        String regexEmail = "^[a-zA-Z0-9._]+@[a-z0-9]+.[a-z]{2,3}+.[a-z]{2,}$";
+        String regexEmail =("^[a-zA-Z0-9._]*@[a-z0-9]{1,}+.[a-z]{2,3}+.[a-z]{2,}$" );
         Pattern p2 = Pattern.compile(regexEmail);
         Matcher m2 = p2.matcher(email);
         boolean r2 = m2.matches();
@@ -62,7 +66,8 @@ public class UserRegistration {
         return r2;
     }
 
-    public static boolean validMobileNumber(String phoneNo)throws InvalidUserDataException {
+    public static boolean validMobileNumber(String phoneNo)
+    {
                 /*System.out.println("Enter your mobile number (eg: Country-code Mobile no - 91 1234567890 ) : ");
                 String phoneNo = scanner.next();*/
 
@@ -76,12 +81,12 @@ public class UserRegistration {
             System.out.println("Phone number is Invalid");
         return r3;
     }
-
-    public static boolean validatePassword(String password)throws InvalidUserDataException {
+    public static boolean validatePassword(String password)
+    {
               /* System.out.println("Enter Password ");
                String password=scanner.next();*/
         //Regular Expression For Validating Password
-        String regexPassword = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8}$";
+        String regexPassword="^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8}$";
 
         /*       *    (?=.*[0-9]) represents a digit must occur at least once.
          *    (?=.*[a-z]) represents a lower case alphabet must occur at least once.
@@ -89,10 +94,10 @@ public class UserRegistration {
          *    (?=.*[@#$%^&-+=()]) represents a special character that must occur at least once.
          *    {8} represents at least 8 characters.
          */
-        Pattern p4 = Pattern.compile(regexPassword);
-        Matcher m4 = p4.matcher(password);
-        boolean r4 = m4.matches();
-        if (r4)
+        Pattern p4=Pattern.compile(regexPassword);
+        Matcher m4=p4.matcher(password);
+        boolean r4=m4.matches();
+        if(r4)
             System.out.println("Password is valid");
         else
             System.out.println("Password is Invalid ");
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -2,127 +2,103 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class UserRegistrationTest {
     UserRegistration userRegistration=new UserRegistration();
 
     @Test
     public void testFor_Valid_FirstName(){
-      try
-      {
-          Assertions.assertTrue(userRegistration.validFirstName("Sneha"));
-          //Assert.assertTrue() methods checks whether the expected value is true or not.
-      } catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+        boolean Firstname= userRegistration.validFirstName("Sneha");
+        //Assert.assertTrue() methods checks whether the expected value is true or not.
+        Assertions.assertTrue(Firstname);
+
     }
 
     @Test
     public void testFor_Valid_LastName(){
-        try
-        {
-            Assertions.assertTrue(userRegistration.validLastName("Bhokare"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+        boolean Lastname= userRegistration.validLastName("Bhokare");
+        Assertions.assertTrue(Lastname);
+
     }
     @Test
     public void testFor_Valid_Email(){
-        try
-        {
-            Assertions.assertTrue(userRegistration.validEmail("bhokare100@gmail.com.net"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+
+        boolean email= userRegistration.validEmail("abc@yahoo.com.net");
+        Assertions.assertTrue(email);
+
     }
 
     @Test
     public void testFor_Valid_Mobilenumber(){
-        try
-        {
-            Assertions.assertTrue(userRegistration.validMobileNumber("91 9762689841"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+
+        boolean phonenumber= userRegistration.validMobileNumber("91 9762689841");
+        Assertions.assertTrue(phonenumber);
+
     }
 
     @Test
     public void testFor_Valid_Password(){
-        try
-        {
-            Assertions.assertTrue(userRegistration.validatePassword("Sneha@12"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+
+        boolean password=userRegistration.validatePassword("Sneha@12");
+        Assertions.assertTrue(password);
     }
     //Tests For Invalid Output
     @Test
     public void testFor_Invalid_FirstName(){
-        try
-        {
-            Assertions.assertFalse(userRegistration.validFirstName("sneha"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+
+        boolean Firstname= userRegistration.validFirstName("sneha");
+        Assertions.assertFalse(Firstname);
+
     }
 
     @Test
     public void testFor_Invalid_LastName(){
-        try
-        {
-            Assertions.assertFalse(userRegistration.validLastName("bhokare"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+        boolean Lastname= userRegistration.validLastName("bhokare");
+        Assertions.assertFalse(Lastname);
+
     }
     @Test
     public void testFor_Invalid_Email(){
-        try
-        {
-            Assertions.assertFalse(userRegistration.validEmail("bhokare100@.com.net"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+
+        boolean email= userRegistration.validEmail("bhokare100@.com.net");
+        Assertions.assertFalse(email);
+
     }
 
     @Test
     public void testFor_Invalid_Mobilenumber(){
-        try
-        {
-            Assertions.assertFalse(userRegistration.validMobileNumber("91 97626899841"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+        boolean phonenumber= userRegistration.validMobileNumber("91 97626899841");
+        Assertions.assertFalse(phonenumber);
+
     }
 
     @Test
     public void testFor_Invalid_Password(){
-        try
-        {
-            Assertions.assertFalse(userRegistration.validatePassword("Sneha@1245"));
-        }
-        catch (InvalidUserDataException e)
-        {
-            System.out.println("Exception Occurs\n"+ e);
-        }
+        boolean password=userRegistration.validatePassword("Sneha@1245");
+        Assertions.assertFalse(password);
     }
+    @Test
+    public void testFor_valid_MultipleEmail(){
+        ArrayList<String> emails =new ArrayList<>(Arrays.asList("abc@yahoo.com.net","abc100@yahoo.com.in","abc.100@yahoo.com.in","abc111@abc.com.edu","abc0@abc.net.com"));
+        for(int i = 0;i< emails.size();i++) {
+            boolean password = userRegistration.validEmail(emails.get(i));
+            Assertions.assertTrue(password);
+        }
+        }
+        @Test
+        public void testFor_Invalid_MultipleEmail(){
+            ArrayList<String> emails =new ArrayList<>(Arrays.asList("abc@.com.my","abc","abc@abc.com","abc@gmail.com.1a","abc0@abc.n"));
+            for(int i = 0;i< emails.size();i++){
+                boolean password=userRegistration.validEmail(emails.get(i));
+                Assertions.assertFalse(password);
 
+            }
+
+
+
+        }
 
 }
 
